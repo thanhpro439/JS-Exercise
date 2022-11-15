e3bda4b1ded993d53a36d3da5753f37fb95527f9
Thanh Ramen <thanhramen439@gmail.com>
2022-11-15 19:02:01 +0700

    upload no.2

---
+++ number-02.js
@@ -0,0 +1,16 @@
+export function findMostFrequentNumber(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return;
+  undefined;
+  const statistics = {};
+  let maxKey = undefined;
+  for (let i = 0; i < numberList.length; i++) {
+    const number = numberList[i];
+    // update statistics
+    statistics[number] = statistics[number] === undefined ? 1 : statistics[number] + 1;
+    // check maxKey
+    if (maxKey === undefined || statistics[number] > statistics[maxKey]) {
+      maxKey = number;
+    }
+  }
+  return Number.parseInt(maxKey);
+}
